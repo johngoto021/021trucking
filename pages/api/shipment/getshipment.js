79cdb52570423490323600ccff96c6a5ae72ac55
jwongrepogit <113310567+jwongrepogit@users.noreply.github.com>
2022-09-16 18:18:27 -0400

    remove shipment folder

--- pages/api/shipment/getshipment.js
+++ pages/api/shipment/getshipment.js
@@ -4,17 +4,34 @@ const prisma = new PrismaClient();
 
 export default async function handler(req, res) {
   
-  const shipments = await prisma.account.findMany({
-    where: {
-      accountId: 6,
-    },
-    select: {
-      accountName: true,
-      
-    },
-    include: {
-      shipments: true,
-    },
+  const shipments = await prisma.shipment.findMany({
+    
+       
+        select: {
+          // This will work!
+          shipmentName: true,
+          shipmentCuid: true,
+          shipmentCustomerRate: true,
+          shipmentCustomerTotalCost: true,
+          
+          accounts: {
+            select: {
+              accountName: true,
+            },
+          },
+          equipmentTypes:{
+            select: {
+              equipmentTypeName: true,
+              
+            }
+          }
+        },
+
+
+
+
+
+    
 });
  
   return res.status(200).json(shipments, { success: true });
