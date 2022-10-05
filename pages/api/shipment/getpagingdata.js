879a3ac2d2bb276a940b8c3870b0440bd890cdea
johngoto021 <john.wong@goto021.com>
2022-10-05 17:01:21 -0400

    updated shipment report

--- pages/api/shipment/getpagingdata.js
+++ pages/api/shipment/getpagingdata.js
@@ -24,6 +24,7 @@ export default async function handler(req, res) {
     internalRate: true,
     shipmentCustomerTotalCost: true,
     internalTotalCost: true,
+    shipmentStatus: true,
 
     accounts: {
       select :{
