b50199fd3696f6e2e225d0758e3e464022b62697
johngoto021 <john.wong@goto021.com>
2022-10-19 18:52:06 -0400

    update account form for autofill region deprecation

--- pages/accounts/[accountcuid].js
+++ pages/accounts/[accountcuid].js
@@ -303,7 +303,6 @@ export default function AccountForm({ accountinfo }) {
                         type="text"
                         name="region"
                         id="region"
-                        autoComplete="region"
                         className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                         defaultValue={accountinfo.region}
                         onChange={(e) => setRegion(e.target.value)}
--- pages/accounts/create.js
+++ pages/accounts/create.js
@@ -277,7 +277,6 @@ export default function AccountForm() {
                         type="text"
                         name="region"
                         id="region"
-                        autoComplete="region"
                         className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                         onChange={(e) => setRegion(e.target.value)}
                         value={region}
