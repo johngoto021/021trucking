1fcae6b758bba6fc48923bf13a0af1c76dd9c644
johngoto021 <john.wong@goto021.com>
2022-10-27 13:53:44 -0400

    removed google map javascript library from app.js

--- pages/shipments/create2.js
+++ pages/shipments/create2.js
@@ -709,7 +709,7 @@ let handleQuoteButtonClick = () => {
               <input type="text" name="locationName" 
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
                 value={element.locationName || ""} onChange={e => handleChange10(index, e)}
-                required="required"  maxLength={128}/>
+                required="required"  maxLength={255} autoComplete="false" />
             </div>
 
             <div className="lg:col-span-3 md:col-span-2 sm:col-span-3">
